f29773b71df84e24d104efae96b2de39acd835b8
unknown <wahyunandanovan@gmail.com>
2023-02-21 10:23:48 +0700

    add env

--- src/core/supabase.jsx
+++ src/core/supabase.jsx
@@ -1,13 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const isDevelopment = import.meta.env.MODE === "development";
-
-const SUPABASE_URL = isDevelopment
-  ? import.meta.env.VITE_SUPABASE_URL
-  : process.env.VITE_SUPABASE_URL;
-const SUPABASE_KEY = isDevelopment
-  ? import.meta.env.VITE_SUPABASE_KEY
-  : process.env.SUPABASE_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
   localStorage: localStorage,
